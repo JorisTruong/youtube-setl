7c6bd3d23e51617e20a0b8e04ee4b54bad80ed4c
Xuzhou Qin <xuzhou.qin@jcdecaux.com>
2020-02-10 10:39:56 +0100

    feat: add App

---
+++ src/main/scala/com/github/joristruong/App.scala
@@ -0,0 +1,41 @@
+package com.github.joristruong
+
+import com.github.joristruong.entity.{Video, VideoCountry, VideoStats}
+import com.github.joristruong.factory.{LatestStatsFactory, VideoFactory}
+import com.jcdecaux.setl.Setl
+
+object App {
+
+  def main(args: Array[String]): Unit = {
+
+    val setl = Setl.builder()
+      .withDefaultConfigLoader()
+      .getOrCreate()
+
+    // Register Repositories
+    setl
+      .setSparkRepository[Video]("videosCARepository", deliveryId = "videosCARepo")
+      .setSparkRepository[Video]("videosDERepository", deliveryId = "videosDERepo")
+      .setSparkRepository[Video]("videosFRRepository", deliveryId = "videosFRRepo")
+      .setSparkRepository[Video]("videosGBRepository", deliveryId = "videosGBRepo")
+      .setSparkRepository[Video]("videosINRepository", deliveryId = "videosINRepo")
+      .setSparkRepository[Video]("videosJPRepository", deliveryId = "videosJPRepo")
+      .setSparkRepository[Video]("videosKRRepository", deliveryId = "videosKRRepo")
+      .setSparkRepository[Video]("videosMXRepository", deliveryId = "videosMXRepo")
+      .setSparkRepository[Video]("videosRURepository", deliveryId = "videosRURepo")
+      .setSparkRepository[Video]("videosUSRepository", deliveryId = "videosUSRepo")
+      .setSparkRepository[VideoCountry]("videosRepository", deliveryId = "videosRepo")
+      .setSparkRepository[VideoStats]("videosStatsRepository", deliveryId = "videosStatsRepo")
+
+    // Register Stages
+    setl
+      .newPipeline()
+      .addStage[VideoFactory]()
+      .addStage[LatestStatsFactory]()
+      .describe()
+      .run()
+      .showDiagram()
+
+  }
+
+}
